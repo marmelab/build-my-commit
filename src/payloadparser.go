461300e173420607aee113e8cf1a73b91977199f
Gildas Garcia <gildas.garcia@gmail.com>
2015-04-20 16:57:21 +0200

    missing files

---
+++ src/payloadparser.go
@@ -0,0 +1,31 @@
+package main
+
+import "encoding/json"
+import "strings"
+import "errors"
+
+const GITHUB_REFERENCE = "refs/heads/master"
+
+type PushEvent struct {
+    Ref string `json:"ref"`
+}
+
+func ParsePayload(payload []byte) (isValid bool, err error) {
+    var pushEvent PushEvent;
+
+    err = json.Unmarshal(payload, &pushEvent)
+
+    if err != nil {
+        return false, err
+    }
+
+    if pushEvent.Ref == "" {
+        return false, errors.New("invalid payload")
+    }
+
+    if !strings.EqualFold(pushEvent.Ref, GITHUB_REFERENCE) {
+        return false, nil
+    }
+
+    return true, nil
+}
