315ae80c9aef878fd082c2747eb779ff11995b57
Gildas Garcia <gildas.garcia@gmail.com>
2015-04-22 14:08:20 +0200

    Fixed tests

--- src/validateRepository_test.go
+++ src/validateRepository_test.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"os"
-	"path"
 	"testing"
 )
 
 func TestValidateRepositoryShouldReturnFalseWhenRepositoryDoesNotContainDockerBuildFile(t *testing.T) {
-	path := "cloned-repository"
-	err := os.Mkdir(path, os.ModeDir)
-
-	if err != nil {
-		t.Errorf("Could not create the test repository")
-	}
-
-	shouldProcess, err := validateRepository("cloned-repository")
+	shouldProcess, err := validateRepository("tests/ValidateRepositoryNoDockerFile")
 
 	if err != nil {
 		t.Errorf("validateRepository() failed with error %q", err)
@@ -23,31 +14,16 @@ func TestValidateRepositoryShouldReturnFalseWhenRepositoryDoesNotContainDockerBu
 	if shouldProcess {
 		t.Errorf("validateRepository() should have returned false")
 	}
-
-	os.RemoveAll(path)
 }
 
 func TestValidateRepositoryShouldReturnTrueWhenRepositoryContainDockerBuildFile(t *testing.T) {
-	directoryPath := "cloned-repository"
-	err := os.Mkdir(directoryPath, os.ModeDir)
-	if err != nil {
-		t.Errorf("Could not create the test repository")
-	}
-
-	_, err = os.Create(path.Join(directoryPath, "build.Dockerfile"))
-	if err != nil {
-		t.Errorf("Could not create the test repository")
-	}
-
-	shouldProcess, err := validateRepository("cloned-repository")
+	shouldProcess, err := validateRepository("tests/ValidateRepositoryWithDockerfile")
 
 	if err != nil {
 		t.Errorf("validateRepository() failed with error %q", err)
 	}
 
 	if !shouldProcess {
-		t.Errorf("validateRepository() should have returned false")
+		t.Errorf("validateRepository() should have returned true")
 	}
-
-	os.RemoveAll(directoryPath)
 }
