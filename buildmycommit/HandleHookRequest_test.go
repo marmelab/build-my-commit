42069d9c70c0587861218e813347315f5533aebc
Gildas <gildas.garcia@gmail.com>
2015-04-28 16:08:07 +0200

    finished tests

--- buildmycommit/HandleHookRequest_test.go
+++ buildmycommit/HandleHookRequest_test.go
@@ -1,9 +1,7 @@
 package buildmycommit
 
 import (
-	//"bytes"
-	//"net/http"
-	//"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/marmelab/buildmycommit/states"
@@ -62,19 +60,116 @@ func TestHandleHookRequestShouldInitializeANewMachineWithStateValidateRequest(t 
 
 func TestHandleHookRequestShouldAddStatesHandlersOnCorrectStates(t *testing.T) {
 	var testMachine TestMachine
+	var errorMessage = "expected %v as handler for state %v, found %v"
 
 	newMachine = func(state int) HookRequestMachine {
-		testMachine = TestMachine{
+		machine := TestMachine{
 			StartState:    state,
 			StateHandlers: map[int]StateHandler{},
 			EndStates:     map[int]bool{}}
 
-		return testMachine
+		testMachine = machine
+		return machine
 	}
 
 	HandleHookRequest(nil, nil)
 
 	if len(testMachine.StateHandlers) != 15 {
 		t.Errorf("testMachine should have %v state handlers", 15)
+	}
+
+	handler := reflect.TypeOf(testMachine.StateHandlers[states.ValidateRequest]).Elem().Name()
+	if handler != "ValidateRequest" {
+		t.Errorf(errorMessage, "ValidateRequest", states.ValidateRequest, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.ParsePayload]).Elem().Name()
+	if handler != "ParsePayload" {
+		t.Errorf(errorMessage, "ParsePayload", states.ParsePayload, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.ValidatePushEvent]).Elem().Name()
+	if handler != "ValidatePushEvent" {
+		t.Errorf(errorMessage, "ValidatePushEvent", states.ValidatePushEvent, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.CloneRepository]).Elem().Name()
+	if handler != "CloneRepository" {
+		t.Errorf(errorMessage, "CloneRepository", states.CloneRepository, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.ValidateDockerFile]).Elem().Name()
+	if handler != "ValidateDockerFile" {
+		t.Errorf(errorMessage, "ValidateDockerFile", states.ValidateDockerFile, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.ValidateCommit]).Elem().Name()
+	if handler != "ValidateCommit" {
+		t.Errorf(errorMessage, "ValidateCommit", states.ValidateCommit, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.BuildDocker]).Elem().Name()
+	if handler != "BuildDocker" {
+		t.Errorf(errorMessage, "BuildDocker", states.BuildDocker, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.RunDocker]).Elem().Name()
+	if handler != "RunDocker" {
+		t.Errorf(errorMessage, "RunDocker", states.RunDocker, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.CompareBuild]).Elem().Name()
+	if handler != "CompareBuild" {
+		t.Errorf(errorMessage, "CompareBuild", states.CompareBuild, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.CommitBuild]).Elem().Name()
+	if handler != "CommitBuild" {
+		t.Errorf(errorMessage, "CommitBuild", states.CommitBuild, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.CheckRemoteHash]).Elem().Name()
+	if handler != "CheckRemoteHash" {
+		t.Errorf(errorMessage, "CheckRemoteHash", states.CheckRemoteHash, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.PushBuild]).Elem().Name()
+	if handler != "PushBuild" {
+		t.Errorf(errorMessage, "PushBuild", states.PushBuild, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.EndRequest]).Elem().Name()
+	if handler != "EndRequest" {
+		t.Errorf(errorMessage, "EndRequest", states.EndRequest, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.CleanRepository]).Elem().Name()
+	if handler != "CleanRepository" {
+		t.Errorf(errorMessage, "CleanRepository", states.CleanRepository, handler)
+	}
+
+	handler = reflect.TypeOf(testMachine.StateHandlers[states.CleanDocker]).Elem().Name()
+	if handler != "CleanDocker" {
+		t.Errorf(errorMessage, "CleanDocker", states.CleanDocker, handler)
+	}
+}
+
+func TestHandleHookRequestShouldAddStateRequestHandledAsEndState(t *testing.T) {
+	var testMachine TestMachine
+
+	newMachine = func(state int) HookRequestMachine {
+		machine := TestMachine{
+			StartState:    state,
+			StateHandlers: map[int]StateHandler{},
+			EndStates:     map[int]bool{}}
+
+		testMachine = machine
+		return machine
+	}
+
+	HandleHookRequest(nil, nil)
+
+	if !testMachine.EndStates[states.RequestHandled] {
+		t.Errorf("End states should be %v", states.RequestHandled)
 	}
 }
